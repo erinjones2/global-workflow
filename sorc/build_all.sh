888fcd8617a97b1a73aca9fae3b062bc0aacc04d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-02-08 11:23:43 -0500

    build enkf_chgres_recenter_nc the old way until nceplibs-ncio is made available in hpc-stack

--- sorc/build_all.sh
+++ sorc/build_all.sh
@@ -146,6 +146,19 @@ if [ -d gfs_wafs.fd ]; then
 fi
 
 #------------------------------------
+# build enkf_chgres_recenter_nc
+#------------------------------------
+$Build_enkf_chgres_recenter_nc && {
+echo " .... Building enkf_chgres_recenter_nc .... "
+./build_enkf_chgres_recenter_nc.sh > $logs_dir/build_enkf_chgres_recenter_nc.log 2>&1
+rc=$?
+if [[ $rc -ne 0 ]] ; then
+    echo "Fatal error in building enkf_chgres_recenter_nc."
+    echo "The log file is in $logs_dir/build_enkf_chgres_recenter_nc.log"
+fi
+((err+=$rc))
+
+#------------------------------------
 # build workflow utilities
 #------------------------------------
 echo " .... Building workflow utilities .... "
